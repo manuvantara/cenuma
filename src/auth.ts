de9eef3d6b85aa833b4a52dc5863cd7b80c2c6cc
dreamqip <v.chuchupak@gmail.com>
2024-03-27 00:21:39 +0100

    feat: switch to prisma adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,13 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import clientPromise from '$lib/mongodb';
+// import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+// import clientPromise from '$lib/mongodb';
+
+const prisma = new PrismaClient();
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: PrismaAdapter(prisma),
   providers: [Google]
 });
